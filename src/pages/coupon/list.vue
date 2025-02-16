efafa18cfd117f9d6e9bb767de860d9cfd631810
wayne23123 <wayne23123@gmail.com>
2025-02-16 16:17:35 +0800

    新增酷碰券api

--- src/pages/coupon/list.vue
+++ src/pages/coupon/list.vue
@@ -1,3 +1,140 @@
+<script setup>
+import {
+  getNoticeList,
+  createNotice,
+  updateNotice,
+  deleteNotice,
+} from '@/api/notice';
+
+import FormDrawer from '@/components/FormDrawer.vue';
+
+import ListHeader from '@/components/ListHeader.vue';
+
+import { useInitTable, useInitForm } from '@/composables/useCommon';
+
+const {
+  tableData,
+  currentPage,
+  total,
+  limit,
+  isLoading,
+  getData,
+  handleDelete,
+} = useInitTable({
+  getList: getNoticeList,
+  delete: deleteNotice,
+});
+
+const {
+  formDrawerRef,
+  formRef,
+  form,
+  rules,
+  drawerTitle,
+  handleSubmit,
+  handleCreate,
+  handleEdit,
+} = useInitForm({
+  form: {
+    title: '',
+    content: '',
+  },
+  rules: {
+    title: [
+      {
+        required: true,
+        message: '公告標題不能為空',
+        trigger: 'blur',
+      },
+    ],
+    content: [
+      {
+        required: true,
+        message: '公告內容不能為空',
+        trigger: 'blur',
+      },
+    ],
+  },
+  //getData 從上面 useInitTable 獲取
+  getData,
+  update: updateNotice,
+  create: createNotice,
+});
+</script>
+
 <template>
-  <div>優惠券列表</div>
+  <!-- rpcard -->
+  <el-card shadow="never" class="border-0">
+    <ListHeader @create="handleCreate" @refresh="getData"></ListHeader>
+
+    <el-table
+      :data="tableData"
+      stripe
+      style="width: 100%"
+      v-loading="isLoading"
+    >
+      <el-table-column prop="title" label="公告標題" />
+      <el-table-column prop="create_time" label="發佈時間" width="300" />
+      <el-table-column label="操作" width="180" align="center">
+        <template #default="scope">
+          <el-button
+            @click="handleEdit(scope.row)"
+            type="primary"
+            text
+            size="small"
+            >修改</el-button
+          >
+
+          <el-popconfirm
+            title="是否要刪除該公告?"
+            confirmButtonText="確定"
+            cancelButtonText="取消"
+            @confirm="handleDelete(scope.row.id)"
+          >
+            <template #reference>
+              <el-button text="true" type="primary" size="small" class="px-1">
+                刪除
+              </el-button>
+            </template>
+          </el-popconfirm>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <div class="flex items-center justify-center mt-5">
+      <el-pagination
+        background
+        layout="prev, pager, next"
+        @current-change="getData"
+        :total="total"
+        v-model:current-page="currentPage"
+        v-model:page-size="limit"
+      />
+    </div>
+
+    <!-- <FormDrawer ref="formDrawerRef" title="新增公告" @submit="handleSubmit"> -->
+    <FormDrawer ref="formDrawerRef" :title="drawerTitle" @submit="handleSubmit">
+      <!-- epf -->
+      <el-form
+        :model="form"
+        ref="formRef"
+        :rules="rules"
+        label-width="80px"
+        :inline="false"
+      >
+        <el-form-item label="公告標題" prop="title">
+          <el-input v-model="form.title" placeholder="公告標題"></el-input>
+        </el-form-item>
+
+        <el-form-item label="公告內容" prop="content">
+          <el-input
+            v-model="form.content"
+            placeholder="公告內容"
+            type="textarea"
+            :row="5"
+          ></el-input>
+        </el-form-item>
+      </el-form>
+    </FormDrawer>
+  </el-card>
 </template>
