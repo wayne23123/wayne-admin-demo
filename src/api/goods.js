4bdc852b0f4d6ff049d675497c5f0e02ebbaacf9
wayne23123 <wayne23123@gmail.com>
2025-02-25 22:19:43 +0800

    新增商品管理-商品管理api

---
+++ src/api/goods.js
@@ -0,0 +1,35 @@
+import axios from '@/axios';
+
+export function getGoodsList(page, query = {}) {
+  let q = [];
+  for (const key in query) {
+    if (query[key]) {
+      q.push(`${key}=${encodeURIComponent(query[key])}`);
+    }
+  }
+
+  let r = q.join('&');
+  r = r ? '?' + r : '';
+
+  return axios.get(`/admin/goods/${page}${r}`);
+}
+
+// 0禁用 1啟用
+export function updateGoodsStatus(ids, status) {
+  return axios.post(`/admin/goods/changestatus`, {
+    ids,
+    status,
+  });
+}
+
+export function createGoods(data) {
+  return axios.post(`/admin/goods`, data);
+}
+
+export function updateGoods(id, data) {
+  return axios.post(`/admin/goods/${id}`, data);
+}
+
+export function deleteGoods(ids) {
+  return axios.post(`/admin/goods/delete_all`, { ids });
+}
