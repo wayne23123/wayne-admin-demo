a63ab71f958089e1b7b6a9e9c3e34a4e16552891
wayne23123 <wayne23123@gmail.com>
2025-04-23 23:52:13 +0800

    新增content.vue組件

---
+++ src/pages/goods/content.vue
@@ -0,0 +1,86 @@
+<script setup>
+import { ref, reactive } from 'vue';
+
+import ChooseImage from '@/components/ChooseImage.vue';
+
+import { readGoods, setGoodsBanner } from '@/api/goods';
+
+import { toast } from '@/composables/util';
+
+const dialogVisible = ref(false);
+
+const form = reactive({
+  banner: [],
+});
+
+const goodsId = ref(0);
+
+const open = (row) => {
+  // console.log('row', row);
+
+  goodsId.value = row.id;
+
+  row.bannersLoading = true;
+
+  readGoods(goodsId.value)
+    .then((response) => {
+      // console.log('response', reponse);
+
+      form.banner = response.data.data.goodsBanner.map((object) => {
+        return object.url;
+      });
+
+      // response.data.data.goodsBanner.map((object) => {
+      //   return object.url;
+      // });
+
+      dialogVisible.value = true;
+    })
+    .finally(() => {
+      row.bannersLoading = false;
+    });
+};
+
+const emit = defineEmits(['reloadData'])
+
+const isLoad = ref(false);
+const submit = () => {
+  isLoad.value = true;
+
+  setGoodsBanner(goodsId.value, form)
+    .then((response) => {
+      toast('設置輪播圖成功');
+
+      dialogVisible.value = false;
+
+      emit('reloadData')
+    })
+    .finally(() => {
+      isLoading.value = false;
+    });
+};
+
+defineExpose({
+  open,
+});
+</script>
+
+<template>
+  <el-drawer
+    title="設置輪播圖"
+    v-model="dialogVisible"
+    size="50%"
+    destroy-on-close
+  >
+    <el-form :model="form" label-width="80px">
+      <el-form-item label="輪播圖">
+        <ChooseImage :limit="9" v-model="form.banner"></ChooseImage>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submit" :loading="isLoad"
+          >提交</el-button
+        >
+      </el-form-item>
+    </el-form>
+  </el-drawer>
+</template>
